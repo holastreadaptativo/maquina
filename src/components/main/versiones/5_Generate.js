1a4d75a2e55b2f1cbfabcdcf1ff6384b180b6054
mgtapia <mgtapia@uc.cl>
2018-03-06 18:46:21 -0300

    diseño generar versiones

--- src/components/main/versiones/5_Generate.js
+++ src/components/main/versiones/5_Generate.js
@@ -5,7 +5,7 @@ import { data } from 'stores'
 export default class Generate extends Component {
 	constructor() {
 		super()
-		this.state = { matrix:[], fns:[], limit:100 }
+		this.state = { fns:[], limit:100 }
 	}
 	componentDidMount() {
 		let fns = []
@@ -16,18 +16,35 @@ export default class Generate extends Component {
 			}
 		})
 	}
-	generate() {
+	handleLimit(e) {
+		this.setState({ limit:e.target.value })
+	}
+	generate(e) {
+		e.preventDefault()
+
 		let matrix = versiones('GEN', { ...this.props, fns:this.state.fns })
 		matrix = matrix.sort(() => (0.5 - Math.random()) ).slice(0, this.state.limit)
 		data.child(`${this.props.code}/versions`).set({ ...matrix })
-		this.setState({ matrix:matrix })
 	}
 	render() {
         return(
 			<aside class={focus(this.props.condition, 'active')}>
-				<div class="title">
-					<h3>Versiones</h3>
-					<button onClick={::this.generate}>Generar</button>
+				<div class="generate">
+					<div class="title">
+						<h3>Versiones</h3>
+					</div>
+					<form>
+						<div class="form-inline">
+							<h5 class="active">Generar 
+								<i class="glyphicon glyphicon-chevron-down"/>
+							</h5>
+							<div class="input-group">
+								<span class="input-group-addon prefix">limite</span>
+								<input defaultValue={this.state.limit} onChange={::this.handleLimit} type="number"/>
+							</div>							
+						</div>
+						<button class="btn" onClick={::this.generate}>Generar</button>
+					</form>
 				</div>
 		    </aside>
 		)
