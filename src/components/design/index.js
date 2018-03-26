f9a8618c54d7c4474527e19572cf15c46e3acf33
mgtapia <mgtapia@uc.cl>
2018-03-26 15:48:50 -0300

    diseño general

--- src/components/design/index.js
+++ src/components/design/index.js
@@ -1,33 +1,20 @@
-import * as geometria from './geometria'
+import * as actions from 'actions'
 import * as datos from './datos'
 import * as general from './general'
-import * as actions from 'actions'
+import * as geometria from './geometria'
 
-export const FUNCIONES = 
-[	
-	{ 
-		name:'General', tag:'general', fns:[
-			{ id:'Insertar Texto', component:general.InsertarTexto, action:actions.insertarTexto },
-			{ id:'Radio Buttons', component:general.RespRadioButtons, action:actions.respRadioBtn }
-		]
-	},
-	{ 
-		name:'Numeración', tag:'numeracion', fns:[]
-	},
-	{ 
-		name:'Álgebra', tag:'algebra', fns:[]
-	},
-	{ 
-		name:'Medición', tag:'medicion', fns:[]
-	},
-	{ 
-		name:'Geometría', tag:'geometria', fns:[
-			{ id:'Plano Cartesiano', component:geometria.PlanoCartesiano, action:actions.planoCartesiano }
-		]
-	},
-	{ 
-		name:'Datos', tag:'datos', fns:[
-			{ id:'Gráfico Datos', component:datos.GraficoDatos, action:actions.graficoDatos }
-		]
-	}
+export const FUNCIONES = [	
+	{ name:'General', tag:'general', fns:[
+		{ id:'Insertar Texto', component:general.InsertarTexto, action:actions.insertarTexto },
+		{ id:'Radio Buttons', component:general.RespRadioButtons, action:actions.respRadioBtn }
+	]},
+	{ name:'Numeración', tag:'numeracion', fns:[]},
+	{ name:'Álgebra', tag:'algebra', fns:[]},
+	{ name:'Medición', tag:'medicion', fns:[]},
+	{ name:'Geometría', tag:'geometria', fns:[
+		{ id:'Plano Cartesiano', component:geometria.PlanoCartesiano, action:actions.planoCartesiano }
+	]},
+	{ name:'Datos', tag:'datos', fns:[
+		{ id:'Gráfico Datos', component:datos.GraficoDatos, action:actions.graficoDatos }
+	]}
 ]
